5288675adc1594acdb81ae3024557bc98dd122fd
AndrewMedvedev42 <sanjino56@gmail.com>
2021-03-18 14:51:42 +0200

    Changed padding of an input field and button, added comments

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import React from "react"
 import Timerz from "./Stopwatch"
 import { FaTimes } from 'react-icons/fa';
 
+
+//Main app
 function App() {
 
   const [name, setName] = React.useState('')
 
+  //creating an array of names that were typed in input field
+
   const [timerNameList, setTimerNameList] = React.useState([])
-
-  function deleteFromLocalStorage(name){
-    localStorage.removeItem(name)
-  }
-
-  const deleteTimer = (name) =>{
-    let newTimers = timerNameList.filter((timerName) => timerName !== name)
-    deleteFromLocalStorage(name)
-    setTimerNameList(newTimers)
-  }
 
   const onChange = (e) =>{
     setName(e.target.value)
   }
-
+//function that checks name and sets new name to array
   const addNewTimer = React.useCallback(() => {
     if(name === ""){
       alert("Input field is empty!")
@@ -29,10 +23,22 @@ function App() {
       alert(`Name: ${name} already exists!`)
     }else{
       setTimerNameList((prevList)=>[name, ...prevList])
-      localStorage.setItem(name, Date.now())
+      localStorage.setItem(name, Date.now())//sets name to local storage(Date.naw() is here just to set dummy value for key))
     }
   },[name, timerNameList])
 
+  //deletes chossed name from array
+
+  const deleteTimer = (name) =>{
+    let newTimers = timerNameList.filter((timerName) => timerName !== name)
+    deleteFromLocalStorage(name)//function that deletes key name from localStorage. Craetion of componets with stored names is comming soon  
+    setTimerNameList(newTimers)
+  }
+
+//deletes keyname 
+  function deleteFromLocalStorage(name){
+    localStorage.removeItem(name)
+  }
 
   return (
     <section >
@@ -49,7 +55,7 @@ function App() {
     </section>
   );
 }
-
+//in "deployedTimers" div the folleowing code creates component for each name in array of names
 
 export default App;
 
